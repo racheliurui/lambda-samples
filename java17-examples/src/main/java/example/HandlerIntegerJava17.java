cc712bd7b8d1d92646b76c6d9115c1a19ca23c6c
Rachel Rui Liu <racliu@amazon.com>
2024-12-02 16:38:03 +0800

    sqs sample

---
+++ java17-examples/src/main/java/example/HandlerIntegerJava17.java
@@ -0,0 +1,31 @@
+package example;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+// Handler value: example.HandlerInteger
+public class HandlerIntegerJava17 implements RequestHandler<SQSEvent, Void> {
+    @Override
+    public Void handleRequest(SQSEvent sqsEvent, Context context) {
+        for (SQSMessage msg : sqsEvent.getRecords()) {
+            processMessage(msg, context);
+        }
+        context.getLogger().log("done");
+        return null;
+    }
+
+    private void processMessage(SQSMessage msg, Context context) {
+        try {
+            context.getLogger().log("Processed message " + msg.getBody());
+
+            // TODO: Do interesting work based on the new message
+
+        } catch (Exception e) {
+            context.getLogger().log("An error occurred");
+            throw e;
+        }
+
+    }
+}
