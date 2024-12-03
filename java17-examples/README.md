1d7a27d36f102c4cb597a977ffd9353a42923bed
Rachel Rui Liu <racliu@amazon.com>
2024-12-03 19:43:41 +0800

    s3 - done

--- java17-examples/README.md
+++ java17-examples/README.md
@@ -2,3 +2,18 @@
 
 Adding Pom to include jars related to secret manager / sqs 
 Adding lambda access
+
+
+# x-ray tracing 
+
+
+START RequestId: 54b8b79a-1b77-47f3-8551-f22128dbb0f9 Version: $LATEST
+File downloaded successfully to /tmp/downloaded_file.txt in 14884  ms
+Dec 03, 2024 11:37:16 AM com.amazonaws.util.Base64 warn
+WARNING: JAXB is unavailable. Will fallback to SDK implementation which may be less performant.If you are using Java 9+, you will need to include javax.xml.bind:jaxb-api as a dependency.
+File uploaded successfully to lambda-artifacts-a2fc6d0a966ac021/sample/250mb.txt.bk in 13989  ms
+END RequestId: 54b8b79a-1b77-47f3-8551-f22128dbb0f9
+REPORT RequestId: 54b8b79a-1b77-47f3-8551-f22128dbb0f9	Duration: 28886.93 ms	Billed Duration: 28887 ms	Memory Size: 2048 MB	Max Memory Used: 420 MB	Init Duration: 1777.67 ms	
+XRAY TraceId: 1-674eed5b-6cb398012915ba2e38b47f0c	SegmentId: 4552303c326b1163	Sampled: true	
+
+https://calculator.aws/#/estimate?id=fd9e10a0515098613e0dd19bfdc3d1bc6331fd79
--- java17-examples/src/main/java/example/HandlerIntegerJava17.java
+++ java17-examples/src/main/java/example/HandlerIntegerJava17.java
@@ -1,31 +1,64 @@
 package example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
 
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
+import java.io.*;
+import java.nio.file.*;
 
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-
-// Handler value: example.HandlerInteger
 public class HandlerIntegerJava17 implements RequestHandler<Object, String> {
-    private static final Logger logger = LogManager.getLogger(HandlerIntegerJava17.class);
+    private static final AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
 
     @Override
     public String handleRequest(Object input, Context context) {
-        LocalDateTime currentTime = LocalDateTime.now();
-        String formattedTime = currentTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-        System.out.println("Current time using system out println: " + formattedTime);
-        context.getLogger().log("Current Date and Time  using lambda log: " + formattedTime);
-        //ERROR StatusLogger Log4j2 could not find a logging implementation. Please add log4j-core to the classpath. Using SimpleLogger to log to the console...
-        logger.info("Current time using log4j: " + formattedTime);
-        return "Lambda function executed successfully.";
+        String bucketName = "lambda-artifacts-a2fc6d0a966ac021";
+        String fileKey = "sample/250mb.txt";
+        String tmpFilePath = "/tmp/downloaded_file.txt";
+        LambdaLogger logger=context.getLogger();
+        try {
+            downloadFile(bucketName, fileKey, tmpFilePath,logger);
+            uploadFile(bucketName, fileKey+".bk", tmpFilePath,logger);
+
+            return "done";
+        } catch (IOException e) {
+            logger.log("Error downloading file");
+            return "Error downloading file: " + e.getMessage();
+        }
+    }
+
+    private String downloadFile(String bucketName, String fileKey, String tmpFilePath,LambdaLogger logger) throws IOException {
+        long startTime = System.currentTimeMillis();
+        try (S3Object s3Object = s3Client.getObject(new GetObjectRequest(bucketName, fileKey));
+        
+
+            InputStream objectData = s3Object.getObjectContent();
+            OutputStream outputStream = Files.newOutputStream(Paths.get(tmpFilePath))) {
+            
+            objectData.transferTo(outputStream);      
+            long endTime = System.currentTimeMillis();
+            long duration = endTime - startTime;      
+            logger.log("File downloaded successfully to " + tmpFilePath + " in " + duration + "  ms");
+            return "File downloaded successfully to " + tmpFilePath;
+        }
+    }
+
+    //add a function to upload file from local to s3 bucket
+    private void uploadFile(String bucketName, String fileKey, String filePath, LambdaLogger logger) throws IOException {
+        long startTime = System.currentTimeMillis();
+        File file = new File(filePath);
+        try {
+            s3Client.putObject(bucketName, fileKey, file);
+            long endTime = System.currentTimeMillis();
+            long duration = endTime - startTime;
+            logger.log("File uploaded successfully to " + bucketName + "/"+ fileKey+ " in " + duration + "  ms");
+        } catch (Exception e) {
+            logger.log("Error uploading file to S3: " + e.getMessage());
+            throw e;
+        }
     }
 }
-    
-
-
