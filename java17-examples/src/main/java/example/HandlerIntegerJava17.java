b90e14df2de3a1102ef61cbd6c412c8f4e3525ed
Rachel Rui Liu <racliu@amazon.com>
2024-12-03 11:36:23 +0800

    secret manager sample done

--- java17-examples/src/main/java/example/HandlerIntegerJava17.java
+++ java17-examples/src/main/java/example/HandlerIntegerJava17.java
@@ -5,10 +5,28 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
+// required when using vault
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
+
+
 // Handler value: example.HandlerInteger
 public class HandlerIntegerJava17 implements RequestHandler<SQSEvent, Void> {
     @Override
     public Void handleRequest(SQSEvent sqsEvent, Context context) {
+
+        // get env varaible called LAMBDA_SECRET and print it out
+        String lambdaSecret = System.getenv("LAMBDA_SECRET");
+        context.getLogger().log("read secret from env LAMBDA_SECRET:  " + lambdaSecret);
+
+
+        // read secret by passing secret manager item and print it out
+        String secretArn = System.getenv("LAMBDA_SECRET_ARN");
+        String secretFromSecretManager = getsecret(secretArn);
+        context.getLogger().log("read secret from reading from secret manager:  " + secretFromSecretManager);
+
         for (SQSMessage msg : sqsEvent.getRecords()) {
             processMessage(msg, context);
         }
@@ -30,4 +48,23 @@ public class HandlerIntegerJava17 implements RequestHandler<SQSEvent, Void> {
         }
 
     }
+
+    public String getsecret(String secretArn) {
+        AWSSecretsManager client = AWSSecretsManagerClientBuilder.defaultClient();
+
+        GetSecretValueRequest getSecretValueRequest = new GetSecretValueRequest()
+                .withSecretId(secretArn);
+        GetSecretValueResult getSecretValueResult = null;
+
+        try {
+            getSecretValueResult = client.getSecretValue(getSecretValueRequest);
+        } catch (Exception e) {
+            throw e;
+        }
+
+        String secret = getSecretValueResult.getSecretString();
+        return secret;
+    }
+
+
 }
